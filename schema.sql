ef11620213f13957c8935a422c8cf44752f11081
Kaiting Chen <ktchen14@gmail.com>
2015-05-18 14:48:57 -0400

    Recomment schema.sql

--- schema.sql
+++ schema.sql
@@ -1,18 +1,20 @@
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
+-- reset the database by dropping and recreating the public schema
+DROP SCHEMA IF NOT EXISTS public CASCADE; CREATE SCHEMA public;
 
---enumerated type person with two possible values
+-- enumeration representing the two people tracked by SettleUp
 CREATE TYPE person AS ENUM ('Kaiting Chen', 'Melanie Plageman');
 
--- persons and associated credit card numbers (last four digits only)
+-- association between credit card last 4s and the owning person
 CREATE TABLE cc_owner (
   cc CHAR(4) PRIMARY KEY, -- the last four digits of the credit card number
   name person NOT NULL -- the name of the person owning the credit card
 );
 
 INSERT INTO cc_owner (cc, name) VALUES
+  -- magic values associated with each person
   ('KMAN', 'Kaiting Chen'),
   ('MELA', 'Melanie Plageman'),
+
   ('1737', 'Kaiting Chen'),
   ('0564', 'Kaiting Chen'),
   ('6532', 'Kaiting Chen'),
@@ -28,12 +30,13 @@ INSERT INTO cc_owner (cc, name) VALUES
   ('8349', 'Melanie Plageman'),
   ('3791', 'Melanie Plageman');
 
--- data per transaction
 CREATE TABLE transactions (
-  amount DECIMAL(8, 2) NOT NULL, -- dollar (or other currency) amount of each transaction
-  merchant_name VARCHAR(255), -- name of merchant
-  cc CHAR(4) NOT NULL REFERENCES cc_owner (cc), -- number of credit card used to transact
-  remote_id VARCHAR(255) PRIMARY KEY, -- unique identifier of the transaction (currently derived from the pdf url)
-  owner person, -- person who owes the amount of the transaction
-  transaction_date date -- date of the transaction
+  amount DECIMAL(8, 2) NOT NULL, -- in USD
+  merchant_name VARCHAR(255),
+  cc CHAR(4) NOT NULL REFERENCES cc_owner, -- last 4 digits of credit card used
+  -- unique id of the transaction (currently derived from the pdf url)
+  remote_id VARCHAR(255) PRIMARY KEY,
+  -- person who owes the amount of the transaction
+  owner person,
+  transaction_date DATE
 );
