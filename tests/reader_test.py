b62d5c003dbaea5465cb55907ac89a0002a87e8b
Melanie Plageman <melanieplageman@gmail.com>
2015-05-18 22:06:54 -0400

    added reader suffix test and cs check label test

--- tests/reader_test.py
+++ tests/reader_test.py
@@ -7,5 +7,9 @@ class TestReaderFunctions(unittest.TestCase):
         row = ['Date', 'Store', 'Total (USD)', 'Payment Type', 'Categories', 'Link', 'Description', 'Vendor']
         self.assertTrue(reader.ShoeboxedCSVReader.is_transaction_prefix(row))
 
+    def test_is_transaction_suffix(self):
+        row = ['', '', '', '', '']
+        self.assertTrue(reader.ShoeboxedCSVReader.is_transaction_suffix(row))
+
 if __name__ == '__main__':
     unittest.main()
